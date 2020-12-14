41ba68131322fa02b0211541f02b5e34e1e867d8
Kim Jun Hyeong <61185968+spy00798@users.noreply.github.com>
2020-12-14 14:55:32 +0900

    findAll 기능 구현

--- src/main/java/com/kimjh/route/User.java
+++ src/main/java/com/kimjh/route/User.java
@@ -1,11 +1,25 @@
 package com.kimjh.route;
 
+import com.kimjh.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/user")
 public class User {
+    private final UserService userService;
+
+    @Autowired
+    public User(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/users")
-    public String users() {
-        return "Users!";
+    public void getUsers() {
+        this.userService.findAll();
+    }
+    @GetMapping("/initialize")
+    public void initializeUsers() {
+        this.userService.initializeUsers();
     }
 }
--- src/main/java/com/kimjh/service/UserService.java
+++ src/main/java/com/kimjh/service/UserService.java
@@ -1,5 +1,6 @@
 package com.kimjh.service;
 
+import com.kimjh.model.User;
 import com.kimjh.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,31 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public void findAll() {
+        for(User user: this.userRepository.findAll() ){
+            System.out.println(user);
+        }
+    }
+
+    public void initializeUsers() {
+
+        User user1 = User.builder()
+                .email("example1@sample.com")
+                .name("Mr. Example")
+                .phone("01000000000")
+                .build();
+        User user2 = User.builder()
+                .email("example2@sample.com")
+                .name("Mr. Sample")
+                .phone("01000001234")
+                .build();
+        User user3 = User.builder()
+                .email("example3@sample.com")
+                .name("Mr. Sample Data")
+                .phone("01012341234")
+                .build();
     }
 
 
